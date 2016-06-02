db170c3674eedc9e2cd00e7a29ab73ad1ff81a1f
Thomas Swann <tkswann@mac.com>
2016-06-02 15:36:52 -0500

    css layout changes

--- songs.js
+++ songs.js
@@ -65,20 +65,60 @@ function hideAddSongView () {
 		addSongView.classList.add('hidden');
 }
 
+function buildSongObj (newSong, newArtist, newAlbum, newGenre) {
+
+	let outputArticle = document.getElementById('mainBody');
+
+	let newObj = {
+								name: newSong,
+								artist: newArtist,
+								album: newAlbum,
+								genre: newGenre
+							};
+
+	songs.push(newObj);	
+	console.log(songs);
+
+	outputArticle.innerHTML += ` 
+						<section class="songSection">
+							<p class="strong">${newObj.name}</p>
+							<p>${newObj.artist}</p>
+								<p><strong>|</strong></p>
+							<p>${newObj.album}</p>
+								<p><strong>|</strong></p>
+							<p>${newObj.genre}</p>
+						</section>`;
+}
+
+function validateFields () {
+
+	let songName = document.getElementById('input-1'),
+	    artist = document.getElementById('input-2'),
+	    album = document.getElementById('input-3'),
+	    genre = document.getElementById('input-4');
+
+	let newSong = songName.value, newArtist = artist.value, newAlbum = album.value, newGenre = genre.value;
+
+			buildSongObj(newSong, newArtist, newAlbum, newGenre);
+
+			songName.value = '';
+			artist.value = '';
+			album.value = '';
+			genre.value = '';
+			
+}
+
+
+
+
+let addSong = document.getElementById('addSongsButton');
+addSong.addEventListener('click', validateFields);
+
 let addLink = document.getElementById('addMusicLink');
 addLink.addEventListener('click', hideListView);
 
 let listLink = document.getElementById('listViewLink');
 listLink.addEventListener('click', hideAddSongView);
-
-
-
-
-
-
-
-
-
 
 
 
